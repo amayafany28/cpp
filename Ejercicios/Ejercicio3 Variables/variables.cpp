a60131ebaa262d40dd19576d111acbcabf0fbf51
Fany Amaya <amaya.fany@unah.hn>
2021-06-17 01:25:26 -0600

    agregando ejercicio 3 y 4

---
+++ Ejercicios/Ejercicio3 Variables/variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numeroEntero;
+    numeroEntero = 15;
+
+    cout <<"Entero: " << numeroEntero << endl;
+
+    numeroEntero =18;
+
+    cout<< "Entero modificado: " << numeroEntero << endl; 
+
+
+    return 0;
+}
